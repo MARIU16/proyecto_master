097129e7ba35e2469127a5d871bff255718492d4
David Renjifo Herrera <David Renjifo Herrera>
2016-06-10 22:15:45 +0200

    funcion listas XD

--- R/proyectomaster.R
+++ R/proyectomaster.R
@@ -1,9 +1,9 @@
-#' Title
-#' 
+#' @title 
 #' @return
 #' @export
 #' @param
 #' @examples
+## descarga la blacklist desde la url y devuelve el resultado en un dataframe
 get_blacklist_abuse<-function() {
   l <- read.table("https://sslbl.abuse.ch/blacklist/sslipblacklist_aggressive.rules", header = TRUE, sep = " ")
   ip<- substring(as.character(l[,6]),2,nchar(as.character(l[,6]))-1)
@@ -13,22 +13,23 @@ get_blacklist_abuse<-function() {
   tabla1<-data.frame(ip_decimal,ip,protocolo,puerto, stringsAsFactors = FALSE)
 }
 
-#' Title
-#' 
+#' @Title
 #' @return
 #' @export
 #' @param
 #' @examples
-show_bl<-function(){
-  View(tabla1)
+## muestra el data frame con la blacklist descargada desde la fuente
+show_blacklist<-function(){
+  bl<-get_blacklist_abuse()
+  View(bl)
 }
 
-#' Title
-#' 
+#' @Title
 #' @return
 #' @export
 #' @param
 #' @examples
+## descarga las ip por paises y lo devuelve en un dataframe
 get_ip_countries<-function(){
   download.file(url="http://geolite.maxmind.com/download/geoip/database/GeoIPCountryCSV.zip", destfile="paises.zip")
   unzip("paises.zip")
@@ -40,49 +41,87 @@ get_ip_countries<-function(){
   tabla2<-data.frame(de1,de2,pa,psa, stringsAsFactors = FALSE)
 }
 
-
-#' Title
-#' 
+#' @Title
 #' @return
 #' @export
 #' @param
 #' @examples
-blacklist_countries<-function(){
-  pais<-matrix(nrow = nrow(tabla1),ncol = 1)
-  paisl<-matrix(nrow = nrow(tabla1),ncol = 1)
-  for (i in 1:nrow(tabla1)){
-    compare <- ((tabla2$de1 <= tabla1$ip_decimal[i]) & (tabla1$ip_decimal[i] <= tabla2$de2))
-    pais[i,1]<-tabla2[compare,4]
-    paisl[i,1]<-tabla2[compare,3]
+## muestra el dataframe con las ip por paises
+show_ip_countries<-function(){
+    ip<-get_ip_countries()
+    View(ip)
+  }
+
+
+#' @Title
+#' @return
+#' @export
+#' @param
+#' @examples
+## añade al blacklis las columnas de los paises segun las ips
+get_blacklist_countries<-function(){
+  bl<-get_blacklist_abuse()
+  bl_c<-get_ip_countries()
+  pais<-matrix(nrow = nrow(bl),ncol = 1)
+  paisl<-matrix(nrow = nrow(bl),ncol = 1)
+  for (i in 1:nrow(bl)){
+    compare <- ((bl_c$de1 <= bl$ip_decimal[i]) & (bl$ip_decimal[i] <= bl_c$de2))
+    pais[i,1]<-bl_c[compare,4]
+    paisl[i,1]<-bl_c[compare,3]
   }
   paisf<-data.frame(pais, paisl)
-  tabla1<-cbind(tabla1,paisf)
+  bl<-cbind(bl,paisf)
 }
 
-#' Title
-#' 
+#' @Title
 #' @return
 #' @export
 #' @param
 #' @examples
-show_bl_coutry<-function(){
-  View(a)
-}
+## muestra el dataframe del blacklist + los paises 
+show_blacklist_coutries<-function(){
+    blc<-get_blacklist_countries()
+    View(blc)
+  }
 
-##creacion de mapa
-#' Title
-#' 
+#' @Title
 #' @return
 #' @export
 #' @param
 #' @examples
+## devuelve en un dataframe la cantida de ip por cada país encontrados en el blacklist
+get_number_blackip_coutry<-function(){
+  bl<-get_blacklist_abuse()
+  bl_c<-get_blacklist_countries()
+  a  <- dplyr::count(bl,bl_c$paisl) 
+}
+
+#' @Title
+#' @return
+#' @export
+#' @param
+#' @examples
+## muestra el data frame con la cantidad de ip por cada país encontrados en el blacklist
+show_number_blackip_coutry<-function(){
+  nbl<-get_number_blackip_coutry()
+  View(nbl)
+}
+
+
+
+#' @Title
+#' @return
+#' @export
+#' @param
+#' @examples
+## creación de mapa
 plot_map<-function(){
   ##total cantidad por pais
-  a  <- dplyr::count(tabla1, paisl) 
+  a<-get_number_blackip_coutry()
   ##crea el mapa
   map  <- joinCountryData2Map(a,
                               joinCode = "ISO2",
-                              nameJoinColumn = "paisl",
+                              nameJoinColumn = "bl_c$paisl",
                               suggestForFailedCodes = T,
                               mapResolution = "coarse",
                               projection = NA, 
@@ -95,9 +134,8 @@ plot_map<-function(){
                             colourPalette = c("Gray87","lightblue1","lightskyblue","royalblue1","royalblue3","royalblue4" ),
                             borderCol="azure4",
                             addLegend=F,
-                            catMethod="dategorical",
-                            
-  )
+                            catMethod="dategorical")
+  
   ##aplica los parametros en el mapa
   do.call( addMapLegend, c( mapParams
                             , legendLabels="all"
